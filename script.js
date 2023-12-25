e56563b996f41907ca243bf8e4f0caa7e27170a7
BesBoo <126841550+BesBoo@users.noreply.github.com>
2023-12-25 12:13:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -89,4 +89,18 @@ cartshow.addEventListener("click",function(){
 cartBtn.addEventListener("click",function(){
     document.querySelector(".cart").style.right="-100%"
 })
-
+//---------------------------- payment------------------------------
+function cardspace(){
+  var carddigit=document.getElementById('cardno').value;
+  if(carddigit.length== 4 || carddigit.length== 9 || carddigit.length== 14){
+    document.getElementById('cardno').value== carddigit+" ";
+    document.getElementById('cardno').max=1;
+  }
+}
+function addSlashes(){
+  var expiredate =document.getElementById('validtil').value;
+  if(expiredate.length== 2){
+    document.getElementById('validtill').value== expiredate+"/";
+    document.getElementById('validtill').max=1;
+  }
+}
