343880ed6d42b9201f1a0086cd6fbf6f0eb3e927
BesBoo <126841550+BesBoo@users.noreply.github.com>
2023-12-28 15:05:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -104,3 +104,40 @@ function addSlashes(){
     document.getElementById('validtill').max=1;
   }
 }
+
+//---------------------------- insert in4 payment-------------------------
+const inputFields = document.querySelectorAll(".payment-content-right-button input");
+const submitButton = document.querySelector(".btn2");
+const notificationContainer = document.querySelector(".payment-content-right");
+
+
+submitButton.addEventListener("click", () => {
+  const emptyInputs = Array.from(inputFields).filter(input => input.value.trim() === "");
+  const visibleEmptyInputs = emptyInputs.filter(input => input.offsetParent !== null);
+
+  
+  if (visibleEmptyInputs.length > 0) {
+    // Display incomplete info notification (red text)
+    const errorNotification = document.createElement("div");
+    errorNotification.classList.add("notification", "error");
+    errorNotification.style.color = "red"; 
+    errorNotification.style.fontSize = "20px";
+    errorNotification.textContent = "Please fill in the following required fields: " + visibleEmptyInputs.map(input => input.placeholder).join(", ");
+    notificationContainer.appendChild(errorNotification);
+    setTimeout(() => errorNotification.remove(), 3000);
+  } else {
+
+    
+    const successNotification = document.createElement("div");
+    successNotification.classList.add("notification", "success");
+    successNotification.style.color = "green"; 
+    successNotification.style.fontSize="20px";
+    successNotification.textContent = "We have received your information. Store staff will contact you soon. Thank you <3";
+    notificationContainer.appendChild(successNotification);
+    setTimeout(() => successNotification.remove(), 10000);
+
+    
+    console.log("Form submitted successfully!");
+  }
+});
+
