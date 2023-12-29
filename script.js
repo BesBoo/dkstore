58be7e7e9d36f07f7e87f0e948d8df1755e98bee
BesBoo <126841550+BesBoo@users.noreply.github.com>
2023-12-29 14:08:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,163 +1,99 @@
-// const btn=document.querySelectorAll(".row button")
-// // console.log(btn)
-// btn.forEach(function(button,index){
-//     // console.log(button,index)
-//     button.addEventListener("click",function(event){
-//         var btnItem = event.target
-//         var product = btnItem.parentElement
-//         var productImg = product.querySelector("img").src
-//         var productName = product.querySelector("h4").innerText
-//         var productPrice = product.querySelector("p span").innerText
-//         addcart(productImg,productName,productPrice)
-//     });
-// });
-// function addcart(productImg,productName,productPrice){
-//     var addtr = document.createElement("tr")
+const btn=document.querySelectorAll(".row button")
+// console.log(btn)
+btn.forEach(function(button,index){
+    // console.log(button,index)
+    button.addEventListener("click",function(event){
+        var btnItem = event.target
+        var product = btnItem.parentElement
+        var productImg = product.querySelector("img").src
+        var productName = product.querySelector("h4").innerText
+        var productPrice = product.querySelector("p span").innerText
+        addcart(productImg,productName,productPrice)
+    });
+});
+function addcart(productImg,productName,productPrice){
+    var addtr = document.createElement("tr")
 
-//     var cartItem = document.querySelectorAll("tbody tr")
-//     for(var i=0;i<cartItem.length;i++){
-//         var productT = document.querySelectorAll(".title")
-//         if(productT[i].innerHTML==productName){
-//             alert("san pham da co trong gio hang")
-//             return
-//         }
-//     }
-//     var trcontent='<tr><td style="display:flex; align-items: center;"><img src="'+productImg+'" style="width:70px;"><span class="title">'+productName+'</span></td><td><span class="prices">'+productPrice+'</span><sup>$</sup></td><td><input style="width: 30px;outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="cart-delete" style="padding:10px;boder:none;border-radius:15px;background:#eb2632;color:white;">Remove</span></td></tr>'
-//     addtr.innerHTML = trcontent
-//     var cartTable = document.querySelector("tbody")
-//     cartTable.append(addtr)
-//     carttotal()
-//     deletecartitem()
-// }
-// //------------------------------------TotalPrices-----------------------------------
-// function carttotal(){
-//     var cartItem = document.querySelectorAll("tbody tr")
-//     // console.log(cartItem)
+    var cartItem = document.querySelectorAll("tbody tr")
+    for(var i=0;i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".title")
+        if(productT[i].innerHTML==productName){
+            alert("san pham da co trong gio hang")
+            return
+        }
+    }
+    var trcontent='<tr><td style="display:flex; align-items: center;"><img src="'+productImg+'" style="width:70px;"><span class="title">'+productName+'</span></td><td><span class="prices">'+productPrice+'</span><sup>$</sup></td><td><input style="width: 30px;outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;"><span class="cart-delete" style="padding:10px;boder:none;border-radius:15px;background:#eb2632;color:white;">Remove</span></td></tr>'
+    addtr.innerHTML = trcontent
+    var cartTable = document.querySelector("tbody")
+    cartTable.append(addtr)
+    carttotal()
+    deletecartitem()
+}
+//------------------------------------TotalPrices-----------------------------------
+function carttotal(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    // console.log(cartItem)
     
-//     // var priceT = document.querySelector(".cart-priceT")
-//     var total = 0
-//     for(var i=0;i<cartItem.length;i++){
-//         var inputValue = cartItem[i].querySelector("input").value
-//         var productPrice = cartItem[i].querySelector(".prices").innerHTML
-//         totalA =inputValue*productPrice
+    // var priceT = document.querySelector(".cart-priceT")
+    var total = 0
+    for(var i=0;i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input").value
+        var productPrice = cartItem[i].querySelector(".prices").innerHTML
+        totalA =inputValue*productPrice
         
-//         // total = Math.round(totalx);
-//         total=total+totalA
+        // total = Math.round(totalx);
+        total=total+totalA
         
-//     }
-//     var cartTotalA = document.querySelector(".price-total span")
-//     cartTotalA.innerHTML=total.toLocaleString('de-DE')
-//     var cartPrice = document.querySelector(".cart-icon span")
-//     cartPrice.innerHTML = total.toLocaleString('de-DE')
-//     // cartTotal.innerHTML = (total*1000).toLocaleString('de-DE')
-//     // priceT.innerHTML=(total*1000).toLocaleString('de-DE')
-//     // deletecartitem()
-//     inputChange()
-// }
-// //-------------------- Delete cart -----------------------------------
-// function deletecartitem(){
-//     var cartItem = document.querySelectorAll("tbody tr")
-//     // var btnItem = document.querySelectorAll(".cart-delete")
-//     for(var i=0;i<cartItem.length;i++){
-//         var productT = document.querySelectorAll(".cart-delete")
-//         productT[i].addEventListener("click",function(event){
-//             var cartDelete = event.target
-//             var cartitemR = cartDelete.parentElement.parentElement
-//             cartitemR.remove()
-//             carttotal()
-//         })
+    }
+    var cartTotalA = document.querySelector(".price-total span")
+    cartTotalA.innerHTML=total.toLocaleString('de-DE')
+    var cartPrice = document.querySelector(".cart-icon span")
+    cartPrice.innerHTML = total.toLocaleString('de-DE')
+    // cartTotal.innerHTML = (total*1000).toLocaleString('de-DE')
+    // priceT.innerHTML=(total*1000).toLocaleString('de-DE')
+    // deletecartitem()
+    inputChange()
+}
+//-------------------- Delete cart -----------------------------------
+function deletecartitem(){
+    var cartItem = document.querySelectorAll("tbody tr")
+    // var btnItem = document.querySelectorAll(".cart-delete")
+    for(var i=0;i<cartItem.length;i++){
+        var productT = document.querySelectorAll(".cart-delete")
+        productT[i].addEventListener("click",function(event){
+            var cartDelete = event.target
+            var cartitemR = cartDelete.parentElement.parentElement
+            cartitemR.remove()
+            carttotal()
+        })
         
-//     }
-// }
+    }
+}
 
+//-----------------------------------------------------------------------
+function inputChange(){
+    var cartItem= document.querySelectorAll("tbody tr")
+    for(var i=0;i<cartItem.length;i++){
+        var inputValue = cartItem[i].querySelector("input")
+        inputValue.addEventListener("change",function(){
+            carttotal()
+        })
+    }
+}
 // //-----------------------------------------------------------------------
-// function inputChange(){
-//     var cartItem= document.querySelectorAll("tbody tr")
-//     for(var i=0;i<cartItem.length;i++){
-//         var inputValue = cartItem[i].querySelector("input")
-//         inputValue.addEventListener("change",function(){
-//             carttotal()
-//         })
-//     }
-// }
-// // //-----------------------------------------------------------------------
-// const cartBtn = document.querySelector(".fa-times")
-// const cartshow = document.querySelector(".fa-shopping-cart")
-// cartshow.addEventListener("click",function(){
-//     document.querySelector(".cart").style.right="0"
-// })
-// cartBtn.addEventListener("click",function(){
-//     document.querySelector(".cart").style.right="-100%"
-// })
+const cartBtn = document.querySelector(".fa-times")
+const cartshow = document.querySelector(".fa-shopping-cart")
+cartshow.addEventListener("click",function(){
+    document.querySelector(".cart").style.right="0"
+})
+cartBtn.addEventListener("click",function(){
+    document.querySelector(".cart").style.right="-100%"
+})
 
 //----------------- test dong bo ----------------------------
 // In index.html:
 
-const btn = document.querySelectorAll(".row button");
-btn.forEach(function(button, index) {
-  button.addEventListener("click", function(event) {
-    var btnItem = event.target;
-    var product = btnItem.parentElement;
-    var productImg = product.querySelector("img").src;
-    var productName = product.querySelector("h4").innerText;
-    var productPrice = product.querySelector("p span").innerText;
-    addcart(productImg, productName, productPrice);
-  });
-});
 
-function addcart(productImg, productName, productPrice) {
-  var addtr = document.createElement("tr");
-
-  var cartItem = document.querySelectorAll("tbody tr");
-  for (var i = 0; i < cartItem.length; i++) {
-    if (cartItem[i].querySelector(".title").innerText === productName) {
-      alert("Sản phẩm đã có trong giỏ hàng");
-      return;
-    }
-  }
-
-  var trcontent = `
-    <tr>
-      <td style="display: flex; align-items: center;">
-        <img src="${productImg}" style="width: 70px;">
-        <span class="title">${productName}</span>
-      </td>
-      <td><span class="prices">${productPrice}</span><sup>$</sup></td>
-      <td><input style="width: 30px; outline: none;" type="number" value="1" min="1"></td>
-      <td style="cursor: pointer;">
-        <span class="cart-delete" style="padding: 10px; boder: none; border-radius: 15px; background: #eb2632; color: white;">Remove</span>
-      </td>
-    </tr>
-  `;
-  addtr.innerHTML = trcontent;
-  var cartTable = document.querySelector("tbody");
-  cartTable.append(addtr);
-
-  // Update total price
-  updateTotal();
-}
-
-function updateTotal() {
-  var cartItem = document.querySelectorAll("tbody tr");
-  var total = 0;
-  for (var i = 0; i < cartItem.length; i++) {
-    var inputValue = cartItem[i].querySelector("input").value;
-    var productPrice = cartItem[i].querySelector(".prices").innerText;
-    total += inputValue * productPrice;
-  }
-
-  // Update total in index.html
-  var cartTotal = document.querySelector(".price-total span");
-  cartTotal.innerHTML = total.toLocaleString("de-DE");
-
-  // Update total in payment.html
-  var paymentTotal = document.querySelector(".price-total span");
-  paymentTotal.innerHTML = total.toLocaleString("de-DE");
-}
-
-// In payment.html:
-
-// No need to change anything here
 
 
 
